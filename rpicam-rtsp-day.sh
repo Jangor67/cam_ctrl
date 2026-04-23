620596c52f993c7d57bf374ff6b0c6fd3443b334
Jan de Gorter <Jan@deGorter.net>
2026-04-23 14:24:41 +0200

    remove option force_key_frames and fix missing backslash

--- rpicam-rtsp-day.sh
+++ rpicam-rtsp-day.sh
@@ -1,10 +1,9 @@
 rpicam-vid \
   -t 0 \
-  --framerate=5
+  --framerate=5 \
   --inline -o - | \
 ffmpeg \
   -re \
   -i - \
   -vcodec copy \
-  -force_key_frames 0:00:05 \
   -f rtsp rtsp://localhost:8554/mystream
--- rpicam-rtsp-midnight.sh
+++ rpicam-rtsp-midnight.sh
@@ -8,5 +8,4 @@ ffmpeg \
   -re \
   -i - \
   -vcodec copy \
-  -force_key_frames 0:00:05 \
   -f rtsp rtsp://localhost:8554/mystream
